03fc96bd41b4e950509e537d02236215712776fa
Matt Dannenberg <dannenberg.matt@gmail.com>
2012-06-04 10:43:47 -0400

    minor fixes/changes from mongovis code review while loops expressed more clearly as for loops redordering writes to disk empty else branch fixed branch order reverse for clarity in a few places extent reordering now quits if handed a negative branch number unnecessary comparator removed

--- src/mongo/tools/vis.cpp
+++ src/mongo/tools/vis.cpp
@@ -32,17 +32,17 @@ namespace po = boost::program_options;
 
 class Vis : public Tool {
 public :
-    Vis() : Tool ( "vis" ) {
+    Vis() : Tool ("vis") {
         add_options()
         ("orderExtent,e", po::value<int>(), "rearrange record pointers so that they are in the same order as they are on disk");
     }
 
     virtual void preSetup() {
         string out = getParam("out");
-        if ( out == "-" ) {
-                // write output to standard error to avoid mangling output
-                // must happen early to avoid sending junk to stdout
-                useStandardOutput(false);
+        if (out == "-") {
+            // write output to standard error to avoid mangling output
+            // must happen early to avoid sending junk to stdout
+            useStandardOutput(false);
         }
     }
 
@@ -50,56 +50,42 @@ public :
         out << "View statistics for data and journal files.\n" << endl;
     }
 
-    // is this the best way to make the set work?
-    struct DiskLocComp {
-        bool operator() (const DiskLoc& lhs, const DiskLoc& rhs) const
-        {return lhs<rhs;}
-    };
+    int reorderExtent(ostream& out, Extent * ex) {
+        set<DiskLoc> dls;
 
-    int reorderExtent( ostream& out, Extent * ex ) {
-        //out << "got to reorderExtent and the extent was passed with size " << ex->length << endl;
-
-        DiskLoc dl = ex->firstRecord;
-        
-        set<DiskLoc, DiskLocComp> dls;
-
-        //out << "extent contents:" << endl;
-        while ( ! dl.isNull() ) {
-            //out << dl.toString() << endl;
+        out << "extent contents:" << endl;
+        for (DiskLoc dl = ex->firstRecord; ! dl.isNull(); dl = dl.rec()->nextInExtent(dl)) {
+            out << dl.toString() << endl;
             dls.insert(dl);
-            dl = dl.rec()->nextInExtent( dl );
         }
 
-        set<DiskLoc, DiskLocComp>::iterator it;
-        //out << "set contents:" << endl;
+        set<DiskLoc>::iterator it;
+        out << "set contents:" << endl;
         DiskLoc prev = DiskLoc();
         DiskLoc cur = DiskLoc();
 
         for (it = dls.begin(); it != dls.end(); it++) {
             prev = cur;
             cur = *it;
-            // dont think if ( prev.isNull() ) is necessary cuz they store nulloffset
-                //set prevOfs = DiskLoc::NullOfs;
-            if ( ! prev.isNull() )
-                prev.rec()->np()->nextOfs = cur.getOfs();
-            else {
-                //out << "new first" << endl;
-                ex->firstRecord = cur;
+            if (prev.isNull()) {
+                out << "new first" << endl;
+                getDur().writingDiskLoc( ex->firstRecord ) = cur;
             }
-            cur.rec()->np()->prevOfs = prev.getOfs();
+            else {
+                getDur().writingInt( prev.rec()->np()->nextOfs ) = cur.getOfs();
+            }
+            getDur().writingInt( cur.rec()->np()->prevOfs ) = prev.getOfs();
 
-            //out << cur.toString() << endl;
+            out << cur.toString() << endl;
         }
-        cur.rec()->np()->nextOfs = DiskLoc::NullOfs;
+        getDur().writingInt( cur.rec()->np()->nextOfs ) = DiskLoc::NullOfs;
 
         // result loop to see if its ordered now..
 
-        dl = ex->firstRecord;
-        
-        //out << "resulting extent contents:"<< endl;
-        while ( ! dl.isNull() ) {
-            //out << dl.toString() << endl;
-            dl = dl.rec()->nextInExtent( dl );
+        //TODO look at geo to see how single file deve blocks work
+        out << "resulting extent contents:"<< endl;
+        for (DiskLoc dl = ex->firstRecord; ! dl.isNull(); dl = dl.rec()->nextInExtent(dl)) {
+            out << dl.toString() << endl;
         }
 
         return 0;
@@ -109,10 +95,10 @@ public :
         string ns;
 
         // TODO allow out to be things from STDOUT
-        ostream &out = cout;
+        ostream& out = cout;
         
 
-        if ( ! hasParam( "dbpath" ) ) {
+        if (! hasParam("dbpath")) {
             out << "mongovis only works with --dbpath" << endl;
             return -1;
         }
@@ -127,65 +113,73 @@ public :
             return -1;
         }
 
-        string dbname = getParam ( "db" );
-        Client::ReadContext cx ( dbname );
-        Database * db = cx.ctx().db();
+        string dbname = getParam ("db");
+        Client::ReadContext cx (dbname);
 
-        NamespaceDetails * nsd = nsdetails( ns.c_str() );
+        NamespaceDetails * nsd = nsdetails(ns.c_str());
+        //TODO make sure nsd is valid/safe/etc
 
-        if ( nsd->firstExtent.isNull() ) {
+        if (nsd->firstExtent.isNull()) {
             out << "ERROR: firstExtent is null" << endl;
             return -1;
         }
 
-        if ( ! nsd->firstExtent.isValid() ) {
+        if (! nsd->firstExtent.isValid()) {
             out << "ERROR: firstExtent is invalid" << endl;
             return -1;
         }
 
-        if ( hasParam( "orderExtent" ) ) {
+        if (hasParam("orderExtent")) {
             int extent_num = getParam("orderExtent", 0);
             Extent * ex = DataFileMgr::getExtent(nsd->firstExtent);
-            out << "extent branch " << extent_num << endl;
+            //out << "extent branch " << extent_num << endl;
+
+            if (extent_num < 0) {
+                out << "ERROR: extent number must be non-negative" << endl;
+            }
 
             for (int i = 1; i < extent_num; i++) {
                 ex = ex->getNextExtent();
-                if ( ex == 0 ) {
+                if (ex == 0) {
                     out << "ERROR: extent " << extent_num << " does not exist" << endl;
                     return -1;
                 }
             }
 
             //out << "size of extent pre passing: " << ex->length << endl;
-            if ( reorderExtent( out, ex ) == 0 ) {
+            if (reorderExtent(out, ex) == 0) {
                 out << "extent " << extent_num << "reordered" << endl;
                 return 0;
             }
-
+            else {
+                out << "An error occurred while repairing extent " << extent_num << endl;
+                return -1;
+            }
         }
 
         
-        Extent * ex = DataFileMgr::getExtent(nsd->firstExtent);
+        
         int extent_num = 0;
 
-        while ( ex != 0 ) { // extent loop
-            extent_num++;
+        for (Extent * ex = DataFileMgr::getExtent(nsd->firstExtent); ex != 0; ex = ex->getNextExtent()) { // extent loop
 
-            DiskLoc dl = ex->firstRecord;
+            
+            //TODO make struct, external struct for real totals, make += op
             int rec_num = 0;
             int total_rec_size = 0;
             int total_bson_size = 0;
+            Record * r;
 
-            while ( ! dl.isNull() ) { // record loop
+            for (DiskLoc dl = ex->firstRecord; ! dl.isNull(); dl = r->nextInExtent(dl)) { // record loop
                 rec_num++;
-                Record * r = dl.rec();
-                //out << "\trecord " << rec_num << ": " << r->lengthWithHeaders() << endl;
+                r = dl.rec();
                 total_rec_size += r->lengthWithHeaders();
                 total_bson_size += dl.obj().objsize();
-                dl = r->nextInExtent( dl );
             }
 
-            out << "extent " << extent_num << ":\n\tsize: " << ex->length 
+            //TODO compute/print stats
+            out << "extent " << extent_num << ':'
+                << "\n\tsize: " << ex->length 
                 << "\n\tnumber of records: " << rec_num 
                 << "\n\tsize used by records: " << total_rec_size 
                 << "\n\tfree by records: " << ex->length - total_rec_size
@@ -194,18 +188,16 @@ public :
                 << "\n\tsize used by BSONObjs: " << total_bson_size 
                 << "\n\tfree by BSON calc: " << ex->length - total_bson_size - 16 * rec_num
                 << "\n\t\% of extent used (BSON): " << (float)total_bson_size / (float)ex->length * 100 
-                << "\n\taverage BSONObj size: " << total_bson_size / rec_num << endl;
+                << "\n\taverage BSONObj size: " << total_bson_size / rec_num 
+                << endl;
 
-            ex = ex->getNextExtent();
+            extent_num++;
         }
-
-        out << "Hello, world!" << endl;
-        
         return 0;
     }
 };
 
-int main( int argc , char ** argv ) {
+int main(int argc, char ** argv) {
     Vis v;
-    return v.main( argc , argv );
+    return v.main(argc, argv);
 }
