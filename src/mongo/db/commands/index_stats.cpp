d7fe8341655a9422a637b3c72c279c45dd0b3979
Andrea Lattuada <andrea.lattuada@10gen.com>
2012-10-03 12:55:01 -0400

    dump V1 trees as JSON

--- src/mongo/db/commands/index_stats.cpp
+++ src/mongo/db/commands/index_stats.cpp
@@ -20,19 +20,15 @@
 #include "mongo/pch.h"
 
 #include <iostream>
-#include <list>
 #include <string>
 
-#include <boost/array.hpp>
-#include <boost/filesystem/convenience.hpp>
-#include <boost/filesystem/operations.hpp>
-
 #include "mongo/client/dbclientcursor.h"
+#include "mongo/db/btree.h"
 #include "mongo/db/commands.h"
 #include "mongo/db/db.h"
+#include "mongo/db/index.h"
 #include "mongo/db/jsobj.h"
 #include "mongo/db/namespace_details.h"
-#include "mongo/util/processinfo.h"
 
 namespace mongo {
 
@@ -72,13 +68,79 @@ namespace {
         return runInternal(errmsg, nsd, result);
     }
 
+    bool inspectBucket(DiskLoc& dl, int depth, BSONObjBuilder& bucketBuilder) {
+        const BtreeBucket<V1>* bucket = dl.btree<V1>();
+        int usedKeyCount = 0;
+        int totalKeyCount = 0;
+        BSONArrayBuilder childsArrayBuilder(bucketBuilder.subarrayStart("childs"));
+        for ( int i = 0; i < bucket->getN(); i++ ) {
+            const BucketBasics<V1>::_KeyNode& kn = bucket->k(i);
+
+            if ( kn.isUsed() ) {
+                ++usedKeyCount;
+            }
+            ++totalKeyCount;
+            if ( !kn.prevChildBucket.isNull() ) {
+                DiskLoc left = kn.prevChildBucket;
+                BSONObjBuilder childBucketBuilder(childsArrayBuilder.subobjStart());
+                inspectBucket(left, depth + 1, childBucketBuilder);
+                childBucketBuilder.doneFast();
+            }
+            //const BtreeBucket *b = left.btree<V>();
+            //if ( strict ) {
+                //verify( b->parent == thisLoc );
+            //}
+            //else {
+                //wassert( b->parent == thisLoc );
+            //}
+            //kc += b->fullValidate(kn.prevChildBucket, order, unusedCount, strict, depth+1);
+            //}
+        }
+        childsArrayBuilder.doneFast();
+        //if ( !this->nextChild.isNull() ) {
+            //DiskLoc ll = this->nextChild;
+            //const BtreeBucket *b = ll.btree<V>();
+            //if ( strict ) {
+                //verify( b->parent == thisLoc );
+            //}
+            //else {
+                //wassert( b->parent == thisLoc );
+            //}
+            //kc += b->fullValidate(this->nextChild, order, unusedCount, strict, depth+1);
+        //}
+        bucketBuilder << "n" << bucket->getN()
+                      << "depth" << depth
+                      << "usedKeys" << usedKeyCount
+                      << "totalKeys" << totalKeyCount;
+        if (bucket->getN() > 0) {
+            const BucketBasics<V1>::KeyNode& firstKeyNode = bucket->keyNode(0);
+            const KeyV1& key = firstKeyNode.key;
+            bucketBuilder.append("firstKey", key.toBson());
+        }
+        return true;
+    }
+
     bool IndexStatsCmd::runInternal(string& errmsg, NamespaceDetails* nsd, BSONObjBuilder& result) {
 
         {
-            BSONArrayBuilder indexesBuilder(result.subarrayStart("indexes"));
+            BSONArrayBuilder indexesArrayBuilder(result.subarrayStart("indexes"));
             for (NamespaceDetails::IndexIterator it = nsd->ii(); it.more(); ) {
                 IndexDetails& details = it.next();
-                indexesBuilder.append(details.indexName());
+                BSONObjBuilder indexBuilder(indexesArrayBuilder.subobjStart());
+                indexBuilder << "name" << details.indexName()
+                             << "version" << details.version()
+                             << "isIdIndex" << details.isIdIndex()
+                             << "keyPattern" << details.keyPattern();
+                IndexInterface& interface = details.idxInterface();
+                interface.fullValidate(details.head, details.keyPattern());
+                if (details.version() != 1) {
+                    return true;
+                }
+                const BtreeBucket<V1>* headBucket = details.head.btree<V1>();
+                {
+                    BSONObjBuilder headBuilder(indexBuilder.subobjStart("head"));
+                    inspectBucket(details.head, 0, headBuilder);
+                }
             }
         }
         return true;
