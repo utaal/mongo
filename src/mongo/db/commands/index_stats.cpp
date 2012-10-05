02590254eab53cb0999972b1c80e6742f8a05f48
Andrea Lattuada <andrea.lattuada@10gen.com>
2012-10-05 16:57:07 -0400

    added storage-related stats, refactor

--- src/mongo/db/commands/index_stats.cpp
+++ src/mongo/db/commands/index_stats.cpp
@@ -2,7 +2,7 @@
  * collection.storageDetails({...}) command
  */
 
-/*    Copyright 2009 10gen Inc.
+/*    Copyright 2012 10gen Inc.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -33,11 +33,12 @@
 namespace mongo {
 
     struct IndexStatsParams {
-        IndexStatsParams() : dumpTree(false) {
+        IndexStatsParams() : dumpTree(false), analyzeStorage(false) {
         }
 
         string indexName;
         bool dumpTree;
+        bool analyzeStorage;
     };
 
     class IndexStatsCmd : public Command {
@@ -101,20 +102,44 @@ namespace mongo {
         }
     };
 
+    inline double average(unsigned int sum, unsigned int count) {
+        return (double) sum / count;
+    }
+
+    class StatsCalc {
+    public:
+        StatsCalc(unsigned int numBuckets, unsigned int bucketBodySize) :
+            _numBuckets(numBuckets), _bucketBodySize(bucketBodySize) {
+        }
+
+        inline double avg(unsigned int val) {
+            return (double) val / _numBuckets;
+        }
+
+        inline double ratio(unsigned int size) {
+            return (double) size / _bucketBodySize / _numBuckets;
+        }
+
+    private:
+        unsigned int _numBuckets;
+        unsigned int _bucketBodySize;
+    };
+
     void LevelStats::appendTo(BSONObjBuilder& builder, const BtreeStats* globalStats) const {
+        StatsCalc calc(numBuckets, globalStats->bucketBodySize);
         builder << "numBuckets" << numBuckets
                 << "usedKeys" << usedKeys
                 << "totalKeys" << totalKeys
-                << "avgUsedKeysPerBucket" << (double) usedKeys / numBuckets
+                << "avgUsedKeysPerBucket" << calc.avg(usedKeys)
                 << "totalBsonSize" << totalBsonSize
-                << "avgBsonSize" << (double) totalBsonSize / numBuckets
+                << "avgBsonSize" << calc.avg(totalBsonSize)
                 << "totalEmptySize" << totalEmptySize
-                << "avgEmptySize" << (double) totalEmptySize / numBuckets
+                << "avgEmptySize" << calc.avg(totalEmptySize)
                 << "totalKeyNodeSize" << totalKeyNodeSize
-                << "avgKeyNodeSize" << (double) totalKeyNodeSize / numBuckets
-                << "avgBsonRatio" << (double) totalBsonSize / globalStats->bucketBodySize / numBuckets
-                << "avgEmptyRatio" << (double) totalEmptySize / globalStats->bucketBodySize / numBuckets
-                << "avgKeyNodesRatio" << (double) totalKeyNodeSize / globalStats->bucketBodySize / numBuckets;
+                << "avgKeyNodeSize" << calc.avg(totalKeyNodeSize)
+                << "bsonRatio" << calc.ratio(totalBsonSize)
+                << "emptyRatio" << calc.ratio(totalEmptySize)
+                << "keyNodesRatio" << calc.ratio(totalKeyNodeSize);
     }
 
     class BtreeInspector {
@@ -168,11 +193,23 @@ namespace mongo {
     }
 
     template <class Version>
+    void addTo(LevelStats& stats, int totalKeyCount, int usedKeyCount, const BtreeBucket<Version>* bucket, int keyNodeSize) {
+        stats.numBuckets += 1;
+        stats.totalKeys += totalKeyCount;
+        stats.usedKeys += usedKeyCount;
+        stats.totalBsonSize += bucket->getBsonSize();
+        stats.totalEmptySize += bucket->getEmptySize();
+        stats.totalKeyNodeSize += keyNodeSize * totalKeyCount;
+    }
+
+    template <class Version>
     bool BtreeInspectorImpl<Version>::inspectBucket(const DiskLoc& dl, unsigned int depth, BSONObjBuilder* bucketBuilder) {
         const BtreeBucket<Version>* bucket = dl.btree<Version>();
         int usedKeyCount = 0;
         int totalKeyCount = 0;
         {
+            killCurrentOp.checkForInterrupt();
+
             scoped_ptr<BSONArrayBuilder> childsArrayBuilder(NULL);
             if (bucketBuilder != NULL) {
                 childsArrayBuilder.reset(new BSONArrayBuilder(bucketBuilder->subarrayStart("childs")));
@@ -190,27 +227,18 @@ namespace mongo {
         }
 
         if (depth > stats.depth) stats.depth = depth;
-        stats.numBuckets += 1;
-        stats.totalKeys += totalKeyCount;
-        stats.usedKeys += usedKeyCount;
-        stats.totalBsonSize += bucket->getBsonSize();
-        stats.totalEmptySize += bucket->getEmptySize();
-        stats.totalKeyNodeSize += sizeof(_KeyNode) * totalKeyCount;
+        addTo(stats, totalKeyCount, usedKeyCount, bucket, sizeof(_KeyNode));
         while (stats.perLevel.size() < depth + 1)
             stats.perLevel.push_back(LevelStats());
         LevelStats& level = stats.perLevel.at(depth);
-        level.numBuckets += 1;
-        level.totalKeys += totalKeyCount;
-        level.usedKeys += usedKeyCount;
-        level.totalBsonSize += bucket->getBsonSize();
-        level.totalEmptySize += bucket->getEmptySize();
-        level.totalKeyNodeSize += sizeof(_KeyNode) * totalKeyCount;
+        addTo(level, totalKeyCount, usedKeyCount, bucket, sizeof(_KeyNode));
 
         if (bucketBuilder != NULL) {
             *bucketBuilder << "n" << bucket->getN()
                            << "depth" << depth
                            << "usedKeys" << usedKeyCount
-                           << "totalKeys" << totalKeyCount;
+                           << "totalKeys" << totalKeyCount
+                           << "diskLoc" << dl.toBSONObj();
         }
 
         if (bucketBuilder != NULL) {
@@ -229,8 +257,8 @@ namespace mongo {
         return true;
     }
 
-    bool IndexStatsCmd::run(const string& dbname, BSONObj& cmdObj, int, string& errmsg,
-                                BSONObjBuilder& result, bool fromRepl) {
+    bool IndexStatsCmd::run(const string& dbname, BSONObj& cmdObj, int, string& errmsg, BSONObjBuilder& result, bool fromRepl) {
+
         string ns = dbname + "." + cmdObj.firstElement().valuestrsafe();
         NamespaceDetails* nsd = nsdetails(ns.c_str());
         if (!cmdLine.quiet) {
@@ -255,6 +283,9 @@ namespace mongo {
         BSONElement dumpTreeElm = cmdObj["dumpTree"];
         if (dumpTreeElm.ok()) params.dumpTree = dumpTreeElm.trueValue();
 
+        BSONElement analyzeStorageElm = cmdObj["analyzeStorage"];
+        if (analyzeStorageElm.ok()) params.analyzeStorage = analyzeStorageElm.trueValue();
+
         return runInternal(errmsg, nsd, params, result);
     }
 
@@ -274,7 +305,47 @@ namespace mongo {
             result << "name" << details.indexName()
                    << "version" << details.version()
                    << "isIdIndex" << details.isIdIndex()
-                   << "keyPattern" << details.keyPattern();
+                   << "keyPattern" << details.keyPattern()
+                   << "storageNs" << details.indexNamespace();
+
+            if (params.analyzeStorage) {
+                NamespaceDetails* indexNsd = nsdetails(details.indexNamespace().c_str());
+                BSONArrayBuilder extentsArrayBuilder(result.subarrayStart("extents"));
+                unsigned int totRecordCount = 0;
+                unsigned int totRecLen = 0;
+                unsigned int totExtentSpace = 0;
+                for (Extent* ex = DataFileMgr::getExtent(indexNsd->firstExtent);
+                     ex != NULL; ex = ex->getNextExtent()) {
+
+                    killCurrentOp.checkForInterrupt();
+
+                    BSONObjBuilder extentBuilder(extentsArrayBuilder.subobjStart());
+                    extentBuilder << "diskLoc" << ex->myLoc.toBSONObj()
+                                  << "length" << ex->length;
+
+                    unsigned int recordCount = 0;
+                    unsigned int recLen = 0;
+                    Record* r;
+                    for (DiskLoc dl = ex->firstRecord; ! dl.isNull(); dl = r->nextInExtent(dl)) {
+                        killCurrentOp.checkForInterrupt();
+                        r = dl.rec();
+
+                        recLen += r->lengthWithHeaders();
+                        totRecLen += r->lengthWithHeaders();
+                        ++recordCount;
+                        ++totRecordCount;
+                    }
+                    totExtentSpace += (ex->length - Extent::HeaderSize());
+
+                    extentBuilder << "entries" << recordCount
+                                  << "recLen" << recLen
+                                  << "usage" << (double) recLen / (ex->length - Extent::HeaderSize());
+                }
+                extentsArrayBuilder.doneFast();
+                result << "numRecords" << totRecordCount
+                       << "overallStorageUsage" << (double) totRecLen / totExtentSpace;
+            }
+
             {
                 scoped_ptr<BSONObjBuilder> headBuilder(NULL);
                 if (params.dumpTree) {
