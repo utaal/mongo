5bc62b809118145c6a4bba2f169c52d2af033879
Andrea Lattuada <andrea.lattuada@10gen.com>
2012-11-13 16:52:36 -0500

    cleaned up test

--- jstests/storageDetailsCommand.js
+++ jstests/storageDetailsCommand.js
@@ -7,90 +7,92 @@ for (var i = 0; i < 3000; ++i) {
     t.insert({i: i, d: i % 13});
 }
 
-var result = t.diskStorageStats({numberOfChunks: 100});
-if (result["bad cmd"]) {
-    print("storageDetails command not available in this build: skipping");
-    quit();
-}
-
-assert(result.ok);
-
-function checkDiskStats(data) {
-    assert(isNumber(data.extentHeaderBytes));
-    assert(isNumber(data.recordHeaderBytes));
-    assert(isNumber(data.numEntries));
-    assert(data.bsonBytes instanceof NumberLong);
-    assert(data.recBytes instanceof NumberLong);
-    assert(data.onDiskBytes instanceof NumberLong);
-    assert(isNumber(data.outOfOrderRecs));
-    assert(isNumber(data.characteristicCount));
-    assert(isNumber(data.characteristicAvg));
-    assert(data.freeRecsPerBucket instanceof Array);
-}
-
-assert(result.extents && result.extents instanceof Array);
-
-var extents = result.extents;
-
-for (var i = 0; i < extents.length; ++i) {
-    assert(isObject(extents[i]));
-    assert.neq(extents[i], null);
-    assert(extents[i].range instanceof Array);
-    assert.eq(extents[i].range.length, 2);
-    assert.eq(extents[i].isCapped, false);
-    checkDiskStats(extents[i]);
-    assert(extents[i].chunks instanceof Array);
-    for (var c = 0; c < extents[i].chunks[c]; ++c) {
-        assert(isObject(extents[i].chunks[c]));
-        assert.neq(extents[i].chunks[c], null);
-        checkStats(extents[i].chunks[c]);
+function test() {
+    var result = t.diskStorageStats({numberOfChunks: 100});
+    if (result["bad cmd"]) {
+        print("storageDetails command not available: skipping");
+        return;
     }
-}
 
-result = t.pagesInRAM({numberOfChunks: 100});
-assert(result.ok);
+    assert.commandWorked(result);
 
-assert(result.extents instanceof Array);
-var extents = result.extents;
-
-for (var i = 0; i < result.extents.length; ++i) {
-    assert(isObject(extents[i]));
-    assert.neq(extents[i], null);
-    assert(isNumber(extents[i].pageBytes));
-    assert(isNumber(extents[i].onDiskBytes));
-    assert(isNumber(extents[i].inMem));
-
-    assert(extents[i].chunks instanceof Array);
-    for (var c = 0; c < extents[i].chunks.length; ++c) {
-        assert(isNumber(extents[i].chunks[c]));
+    function checkDiskStats(data) {
+        assert(isNumber(data.extentHeaderBytes));
+        assert(isNumber(data.recordHeaderBytes));
+        assert(isNumber(data.numEntries));
+        assert(data.bsonBytes instanceof NumberLong);
+        assert(data.recBytes instanceof NumberLong);
+        assert(data.onDiskBytes instanceof NumberLong);
+        assert(isNumber(data.outOfOrderRecs));
+        assert(isNumber(data.characteristicCount));
+        assert(isNumber(data.characteristicAvg));
+        assert(data.freeRecsPerBucket instanceof Array);
     }
+
+    assert(result.extents && result.extents instanceof Array);
+
+    var extents = result.extents;
+
+    for (var i = 0; i < extents.length; ++i) {
+        assert(isObject(extents[i]));
+        assert.neq(extents[i], null);
+        assert(extents[i].range instanceof Array);
+        assert.eq(extents[i].range.length, 2);
+        assert.eq(extents[i].isCapped, false);
+        checkDiskStats(extents[i]);
+        assert(extents[i].chunks instanceof Array);
+        for (var c = 0; c < extents[i].chunks[c]; ++c) {
+            assert(isObject(extents[i].chunks[c]));
+            assert.neq(extents[i].chunks[c], null);
+            checkStats(extents[i].chunks[c]);
+        }
+    }
+
+    result = t.pagesInRAM({numberOfChunks: 100});
+    assert(result.ok);
+
+    assert(result.extents instanceof Array);
+    var extents = result.extents;
+
+    for (var i = 0; i < result.extents.length; ++i) {
+        assert(isObject(extents[i]));
+        assert.neq(extents[i], null);
+        assert(isNumber(extents[i].pageBytes));
+        assert(isNumber(extents[i].onDiskBytes));
+        assert(isNumber(extents[i].inMem));
+
+        assert(extents[i].chunks instanceof Array);
+        for (var c = 0; c < extents[i].chunks.length; ++c) {
+            assert(isNumber(extents[i].chunks[c]));
+        }
+    }
+
+    function checkErrorConditions(helper) {
+        var result = helper.apply(t, [{extent: 'a'}]);
+        assert.commandFailed(result);
+        assert(result.errmsg.match(/extent.*must be a number/));
+
+        result = helper.apply(t, [{range: [2, 4]}]);
+        assert.commandFailed(result);
+        assert(result.errmsg.match(/range is only allowed.*extent/));
+
+        result = helper.apply(t, [{extent: 3, range: [3, 'a']}]);
+        assert.commandFailed(result);
+        assert(result.errmsg.match(/must be an array.*numeric elements/));
+
+        result = helper.apply(t, [{granularity: 'a'}]);
+        assert.commandFailed(result);
+        assert(result.errmsg.match(/granularity.*number/));
+
+        result = helper.apply(t, [{numberOfChunks: 'a'}]);
+        assert.commandFailed(result);
+        assert(result.errmsg.match(/numberOfChunks.*number/));
+
+        result = helper.apply(t, [{extent: 100}]);
+        assert.commandFailed(result);
+        assert(result.errmsg.match(/extent.*does not exist/));
+    }
+
+    checkErrorConditions(t.diskStorageStats);
+    checkErrorConditions(t.pagesInRAM);
 }
-
-function checkErrorConditions(helper) {
-    var result = helper.apply(t, [{extent: 'a'}]);
-    assert(!result.ok);
-    assert(result.errmsg.match(/extent.*must be a number/));
-
-    result = helper.apply(t, [{range: [2, 4]}]);
-    assert(!result.ok);
-    assert(result.errmsg.match(/range is only allowed.*extent/));
-
-    result = helper.apply(t, [{extent: 3, range: [3, 'a']}]);
-    assert(!result.ok);
-    assert(result.errmsg.match(/must be an array.*numeric elements/));
-
-    result = helper.apply(t, [{granularity: 'a'}]);
-    assert(!result.ok);
-    assert(result.errmsg.match(/granularity.*number/));
-
-    result = helper.apply(t, [{numberOfChunks: 'a'}]);
-    assert(!result.ok);
-    assert(result.errmsg.match(/numberOfChunks.*number/));
-
-    result = helper.apply(t, [{extent: 100}]);
-    assert(!result.ok);
-    assert(result.errmsg.match(/extent.*does not exist/));
-}
-
-checkErrorConditions(t.diskStorageStats);
-checkErrorConditions(t.pagesInRAM);
