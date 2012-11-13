a41e732fa0c97ec647e2b3a20d3166ce8923ed67
Andrea Lattuada <andrea.lattuada@10gen.com>
2012-11-13 11:38:57 -0500

    review

--- src/mongo/util/processinfo.h
+++ src/mongo/util/processinfo.h
@@ -115,18 +115,17 @@ namespace mongo {
          * NOTE requires blockCheckSupported() == true
          */
         inline static const void* alignToStartOfPage(const void* ptr) {
-            return reinterpret_cast<void*>(
-                    reinterpret_cast<unsigned long long>(ptr) & ~(getPageSize() - 1));
+            return reinterpret_cast<const void*>(
+                    reinterpret_cast<size_t>(ptr) & ~(getPageSize() - 1));
         }
 
         /**
          * Sets i-th element of 'out' to non-zero if the i-th page starting from the one containing
          * 'start' is in memory.
-         * The 'out' vector will be resized (expanded or shrinked) to fit the requested number of
-         * pages.
+         * The 'out' vector will be resized to fit the requested number of pages.
          * @return true on success, false otherwise
          *
-         * NOTE: requires out.size() >= numPages and blockCheckSupported() == true
+         * NOTE: requires blockCheckSupported() == true
          */
         static bool pagesInMemory(const void* start, size_t numPages, vector<char>* out);
 
